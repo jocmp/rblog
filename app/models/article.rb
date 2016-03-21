97ae8a56fe2fdd1d671b6503d5de29b1eb51f5a6
Josiah Campbell <josiahcampbell64@gmail.com>
2016-03-20 23:58:49 -0400

    Added comments model. Left off here:
    * `You'll see a bit more complexity
    here than you did in the controller for articles...`

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 # `Class names in Ruby must begin with a capital letter.
 class Article < ActiveRecord::Base
+  has_many :comments
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
     resources :articles
 
+    resources :articles do
+      resources :comments
+    end
+    
     root 'welcome#index'
   end
   # Example of regular route:
